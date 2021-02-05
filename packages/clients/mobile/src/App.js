599022ddf1a29227f480773595fd3a888ee4747e
Arthur Santos <silva.arthur@academico.ifpb.edu.br>
2021-02-04 21:18:11 -0300

    chore: add permission request on iOS

--- packages/clients/mobile/src/App.js
+++ packages/clients/mobile/src/App.js
@@ -18,6 +18,21 @@ const App = () => {
     return unsubscribe
   }, [])
 
+  useEffect(() => {
+    async function requestUserPermission() {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    }
+
+    requestUserPermission()
+  }, [])
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
